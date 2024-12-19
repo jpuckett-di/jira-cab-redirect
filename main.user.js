f4606320346a2541b930c48f5802676eb74421a0
Jeff Puckett <jpuckett@dealerinspire.com>
2024-12-19 11:18:38 +0000

    add new service desk URI

--- main.user.js
+++ main.user.js
@@ -2,11 +2,22 @@
 // @name        JIRA Redirect CAB links
 // @namespace   Violentmonkey Scripts
 // @match       https://carscommerce.atlassian.net/servicedesk/customer/portal/41/*
+// @match       https://carscommerce.atlassian.net/jira/servicedesk/projects/CAB/queues/issue/*
 // @grant       none
 // @version     1.0
 // @author      -
 // @description 7/19/2024, 2:08:47 PM
 // ==/UserScript==
-const find = '/servicedesk/customer/portal/41'
+const find = [
+    '/servicedesk/customer/portal/41',
+    '/jira/servicedesk/projects/CAB/queues/issue',
+]
 const replace = '/browse'
-window.location.replace(window.location.href.replace(find, replace))
+
+let destination = window.location.href
+
+for (let index = 0; index < find.length; index++) {
+    destination = destination.replace(find[index], replace)
+}
+
+window.location.replace(destination)
