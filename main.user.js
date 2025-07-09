51bdb9a0941b458c1608a00e53c0bd1ae85b34fc
Jeff Puckett <jpuckett@dealerinspire.com>
2025-07-09 20:17:12 +0000

    add PIESD

--- main.user.js
+++ main.user.js
@@ -1,23 +1,24 @@
 // ==UserScript==
-// @name        JIRA Redirect CAB links
+// @name        JIRA Redirect ServiceDesk links
 // @namespace   Violentmonkey Scripts
 // @match       https://carscommerce.atlassian.net/servicedesk/customer/portal/41/*
+// @match       https://carscommerce.atlassian.net/servicedesk/customer/portal/61/*
 // @match       https://carscommerce.atlassian.net/jira/servicedesk/projects/CAB/queues/issue/*
 // @grant       none
-// @version     1.0
-// @author      -
-// @description 7/19/2024, 2:08:47 PM
+// @version     1.0.1
+// @author      Jeff Puckett
 // ==/UserScript==
 const find = [
-    '/servicedesk/customer/portal/41',
-    '/jira/servicedesk/projects/CAB/queues/issue',
-]
-const replace = '/browse'
+  "/servicedesk/customer/portal/41",
+  "/servicedesk/customer/portal/61",
+  "/jira/servicedesk/projects/CAB/queues/issue",
+];
+const replace = "/browse";
 
-let destination = window.location.href
+let destination = window.location.href;
 
 for (let index = 0; index < find.length; index++) {
-    destination = destination.replace(find[index], replace)
+  destination = destination.replace(find[index], replace);
 }
 
-window.location.replace(destination)
+window.location.replace(destination);
